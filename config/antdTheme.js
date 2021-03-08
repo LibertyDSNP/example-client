8f47d8dcc81eb288977d068de7a3b19dbc431c91
Joshua Rothfus <joshrothfus@gmail.com>
2021-03-08 10:48:52 -0600

    Fixed Logo, added config settings

---
+++ config/antdTheme.js
@@ -0,0 +1,14 @@
+// List of default values can be found here:
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+
+// Changes require a webpack restart
+
+module.exports = {
+  "avatar-size-base":         "48px",
+  "avatar-size-lg":           "64px",
+  "layout-body-background":   "none",
+  "layout-header-background": "none",
+  "layout-sider-background":  "white",
+  "layout-header-height":     "128px",
+  "menu-item-border":         "0px",
+};
