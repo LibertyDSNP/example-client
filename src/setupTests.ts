8ed1a198f36d969cee325d84967a3a7d8125591d
V. Claire Olmstead <viol5229@colorado.edu>
2021-03-18 11:37:06 -0700

    resolve wils comments

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,4 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new Adapter() });
