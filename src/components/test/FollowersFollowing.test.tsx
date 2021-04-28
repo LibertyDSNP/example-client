77f1ddb74891b901ecc550df31320d0c2c0a1735
V. Claire Olmstead <viol5229@colorado.edu>
2021-04-27 17:08:20 -0700

    added more testing and fixed async issues on followersfollowing

--- src/components/test/FollowersFollowing.test.tsx
+++ src/components/test/FollowersFollowing.test.tsx
@@ -1,17 +1,59 @@
 import FollowersFollowing from "../FollowersFollowing";
 import { mount } from "enzyme";
+import { forcePromiseResolve } from "../../test/testhelpers";
 import { componentWithStore, createMockStore } from "../../test/testhelpers";
 import { getPreFabSocialGraph } from "../../test/testGraphs";
 import { getPrefabSocialAddress } from "../../test/testAddresses";
 
+const profile = getPrefabSocialAddress(0);
+const graph = getPreFabSocialGraph().get(profile);
+const userGraph = { user: { graph } };
+const store = createMockStore(userGraph);
+
 describe("FollowersFollowing", () => {
-  it("renders without crashing", () => {
-    const profile = getPrefabSocialAddress(0);
-    const graph = getPreFabSocialGraph().get(profile);
-    const userGraph = { user: { graph } };
-    const store = createMockStore(userGraph);
-    expect(() => {
-      mount(componentWithStore(FollowersFollowing, store));
-    }).not.toThrow();
+  it("renders without crashing", async () => {
+    const component = mount(componentWithStore(FollowersFollowing, store));
+    await forcePromiseResolve();
+    expect(() => component).not.toThrow();
+  });
+
+  describe("button displays correct list title", () => {
+    it("displays correct list title on followers click", async () => {
+      const component = mount(componentWithStore(FollowersFollowing, store));
+      await forcePromiseResolve();
+      component.find(".FollowersFollowing__button").first().simulate("click");
+      expect(component.find("FollowersFollowingUsers").prop("listStatus")).toBe(
+        1
+      );
+    });
+
+    it("displays correct list title on following click", async () => {
+      const component = mount(componentWithStore(FollowersFollowing, store));
+      await forcePromiseResolve();
+      component.find(".FollowersFollowing__button").last().simulate("click");
+      expect(component.find("FollowersFollowingUsers").prop("listStatus")).toBe(
+        2
+      );
+    });
+
+    it("hides list on double click", async () => {
+      const component = mount(componentWithStore(FollowersFollowing, store));
+      await forcePromiseResolve();
+      component.find(".FollowersFollowing__button").last().simulate("click");
+      component.find(".FollowersFollowing__button").last().simulate("click");
+      expect(component.find("FollowersFollowingUsers").prop("listStatus")).toBe(
+        0
+      );
+    });
+
+    it("switches back and forth between lists", async () => {
+      const component = mount(componentWithStore(FollowersFollowing, store));
+      await forcePromiseResolve();
+      component.find(".FollowersFollowing__button").first().simulate("click");
+      component.find(".FollowersFollowing__button").last().simulate("click");
+      expect(component.find("FollowersFollowingUsers").prop("listStatus")).toBe(
+        2
+      );
+    });
   });
 });
--- src/components/test/FollowersFollowingUsers.test.tsx
+++ src/components/test/FollowersFollowingUsers.test.tsx
@@ -24,14 +24,35 @@ describe("FollowersFollowingUsers", () => {
     }).not.toThrow();
   });
 
-  it("displays correct follow or unfollow button", () => {
+  it("displays correct follow button", () => {
     const component = shallow(
       <FollowersFollowingUsers
         listStatus={ListStatus.FOLLOWERS}
         tempUserList={mockTempUserList}
-        notFollowing={[preFabProfiles[1]]}
+        notFollowing={[preFabProfiles[0]]}
       />
     );
-    expect(component).toMatchSnapshot();
+    expect(
+      component.find(".FollowersFollowingUsers__button").first().text()
+    ).toContain("Follow");
+  });
+
+  it("displays correct unfollow button", () => {
+    const component = shallow(
+      <FollowersFollowingUsers
+        listStatus={ListStatus.FOLLOWERS}
+        tempUserList={mockTempUserList}
+        notFollowing={[preFabProfiles[0]]}
+      />
+    );
+    expect(
+      component.find(".FollowersFollowingUsers__button").at(1).text()
+    ).toContain("Unfollow");
+    expect(
+      component.find(".FollowersFollowingUsers__button").at(2).text()
+    ).toContain("Unfollow");
+    expect(
+      component.find(".FollowersFollowingUsers__button").at(3).text()
+    ).toContain("Unfollow");
   });
 });
