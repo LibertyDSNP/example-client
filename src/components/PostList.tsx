2513b459d8d815adc4f31d69578ba00890ea0f85
V. Claire Olmstead <viol5229@colorado.edu>
2021-05-11 11:26:49 -0700

    new post modal and styling

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,9 +4,8 @@ import { FeedItem } from "../utilities/types";
 import { useAppSelector } from "../redux/hooks";
 
 const PostList: React.FC = () => {
-  const feed: FeedItem[] = useAppSelector((state) => state.feed.feed).filter(
-    (post) => post?.content?.type === "Note"
-  );
+  const feed: FeedItem[] = useAppSelector((state) => state.feed.feed);
+  feed.filter((post) => post?.content?.type === "Note");
 
   const postList = feed
     .slice(0)
