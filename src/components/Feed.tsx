d3146eda69194b9912671185d5173a4b718a7e5b
V. Claire Olmstead <viol5229@colorado.edu>
2021-06-24 15:42:27 -0700

    edits made

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -6,11 +6,7 @@ import { useAppSelector } from "../redux/hooks";
 
 const Feed: React.FC = () => {
   const profile = useAppSelector((state) => state.user.profile);
-<<<<<<< HEAD
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-=======
-  const [openCreatePost, setCreatePost] = useState<boolean>(false);
->>>>>>> c0a25e5 (new post modal and styling)
 
   return (
     <div className="Feed__block">
@@ -19,26 +15,15 @@ const Feed: React.FC = () => {
         {profile && (
           <Button
             className="Feed__newPostButton"
-<<<<<<< HEAD
             onClick={() => setIsModalOpen(true)}
-=======
-            onClick={() => setCreatePost(true)}
->>>>>>> c0a25e5 (new post modal and styling)
           >
             New Post
           </Button>
         )}
-<<<<<<< HEAD
         {isModalOpen && (
           <NewPost
             onSuccess={() => setIsModalOpen(false)}
             onCancel={() => setIsModalOpen(false)}
-=======
-        {openCreatePost && (
-          <NewPost
-            onSuccess={() => setCreatePost(false)}
-            onCancel={() => setCreatePost(false)}
->>>>>>> c0a25e5 (new post modal and styling)
           />
         )}
       </div>
