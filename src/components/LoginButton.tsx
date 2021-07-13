0602ad95fe09d672140de038bd998fab1cee7e60
V. Claire Olmstead <viol5229@colorado.edu>
2021-07-13 12:20:41 -0700

    styling put in and toggle if metamask is not installed

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,73 @@
+import { Button, Popover, Spin } from "antd";
+import * as wallet from "../services/wallets/wallet";
+import React from "react";
+
+interface LoginButtonProps {
+  popoverVisible: boolean;
+  setPopoverVisible: any;
+  loginWalletOptions: wallet.WalletType;
+  loading: boolean;
+  onButtonClick: (wallet: wallet.WalletType) => void;
+}
+
+const LoginButton = ({
+  popoverVisible,
+  setPopoverVisible,
+  loginWalletOptions,
+  loading,
+  onButtonClick,
+}: LoginButtonProps): JSX.Element => {
+  const handleVisibleChange = (visible: boolean) => {
+    setPopoverVisible(visible);
+  };
+
+  const getHeaderLoginButton = (
+    <Popover
+      placement="bottomRight"
+      trigger="click"
+      visible={popoverVisible}
+      onVisibleChange={handleVisibleChange}
+      content={
+        <div className="LoginButton__loginOptions">
+          <Button
+            className="LoginButton__loginTorus"
+            onClick={() => onButtonClick(wallet.WalletType.TORUS)}
+          >
+            Torus
+          </Button>
+          <Button
+            className="LoginButton__loginMetamask"
+            onClick={() => onButtonClick(wallet.WalletType.METAMASK)}
+          >
+            MetaMask
+          </Button>
+        </div>
+      }
+    >
+      <Button className="LoginButton__loginButton" aria-label="Login">
+        Log In
+        {loading && <Spin className="LoginButton__spinner" size="small" />}
+      </Button>
+    </Popover>
+  );
+
+  const getQuickStartLoginButton = (
+    <Button
+      className="LoginButton__loginButton LoginButton__loginButton--quickStart"
+      aria-label="Login"
+      onClick={() => onButtonClick(loginWalletOptions)}
+    >
+      Log In With {loginWalletOptions}
+      {loading && <Spin className="LoginButton__spinner" size="small" />}
+    </Button>
+  );
+
+  return (
+    <>
+      {loginWalletOptions === wallet.WalletType.NONE
+        ? getHeaderLoginButton
+        : getQuickStartLoginButton}
+    </>
+  );
+};
+export default LoginButton;
--- src/components/ProfileBlock.tsx
+++ src/components/ProfileBlock.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import * as wallet from "../services/wallets/wallet";
 import { Button } from "antd";
 import { useAppSelector } from "../redux/hooks";
 import ConnectionsList from "./ConnectionsList";
 import UserAvatar from "./UserAvatar";
 import { Profile } from "../utilities/types";
 import { Tabs } from "antd";
+import { isInstalled } from "../services/wallets/metamask/metamask";
+import Login from "./Login";
 
 const ProfileBlock = (): JSX.Element => {
   const profile: Profile | undefined = useAppSelector(
@@ -112,10 +115,86 @@ const ProfileBlock = (): JSX.Element => {
         </>
       ) : (
         <>
-          <div className="ProfileBlock__loginText"> Login Quick Start</div>
+          <div className="ProfileBlock__loginHeaderText">
+            {" "}
+            Login Quick Start
+          </div>
+          <p>
+            To use the Example Client, you must log in with either MetaMask or
+            Torus. Read the guides below to get started!
+          </p>
           <Tabs defaultActiveKey="1">
-            <Tabs.TabPane tab="MetaMask" key="1"></Tabs.TabPane>
-            <Tabs.TabPane tab="Taurus" key="2"></Tabs.TabPane>
+            <Tabs.TabPane tab="MetaMask" key="1">
+              <div className="ProfileBlock__loginTitleText">
+                MetaMask Log In Guide
+              </div>
+              {isInstalled() ? (
+                <>
+                  <p>
+                    You already have MetaMask installed! Just click the{" "}
+                    <code>Log In &#8594; MetaMask</code> button.
+                  </p>
+                  <Login loginWalletOptions={wallet.WalletType.METAMASK} />
+                </>
+              ) : (
+                <>
+                  <p>
+                    Before we can access the browser we need to install MetaMask
+                    to give us a cryptowallet. MetaMask is a browser extension
+                    Chrome or Firefox are recommended. Download them from your
+                    chosen browsers extension manager. Pin MetaMask to your
+                    extension bar.
+                  </p>
+                  <p>
+                    Once{" "}
+                    <a
+                      href="https://metamask.io/download.html"
+                      target="__blank"
+                    >
+                      installed correctly
+                    </a>
+                    , setup MetaMask by following these steps:
+                  </p>
+                  <ul>
+                    <li>Use an existing account do not create a new one.</li>
+                    <li>
+                      For account data use the following seed:{" "}
+                      <code>
+                        stove ankle number crucial clay heavy toilet entire bid
+                        betray cluster degree
+                      </code>
+                    </li>
+                    <li>
+                      Connect to a new Custom RLC.
+                      <ul>
+                        <li>Connect to a new Custom RLC.</li>
+                        <li>
+                          Name: <code>???</code>
+                        </li>
+                        <li>
+                          New RPC URL: <code>http://localhost:7545</code>
+                        </li>
+                        <li>
+                          ChainID: <code>1337</code>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>Once setup, select/connect to that Custom RPC.</li>
+                  </ul>
+                  <Login loginWalletOptions={wallet.WalletType.METAMASK} />
+                </>
+              )}
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Torus" key="2">
+              <div className="ProfileBlock__loginTitleText">
+                Torus Log In Guide
+              </div>
+              <p>
+                There is no setup required for Torus. Just click the{" "}
+                <code>Log In &#8594; Torus</code> button.
+              </p>
+              <Login loginWalletOptions={wallet.WalletType.TORUS} />
+            </Tabs.TabPane>
           </Tabs>
         </>
       )}
