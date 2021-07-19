0973893d3d9ddb4a4f7234dc048796958a317553
acruikshank <acruikshank@example.com>
2021-07-19 12:39:11 -0400

    remove debug code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,8 @@ import * as wallet from "../services/wallets/wallet";
 const Header = (): JSX.Element => {
   return (
     <div className="Header__block">
-<<<<<<< HEAD
       <h1 className="Header__title">Example Client</h1>
       <Login loginWalletOptions={wallet.WalletType.NONE} />
-=======
-      <h1 className="Header__title">Example Client {process.env.RPC_URL}</h1>
-      <Login />
->>>>>>> round trip DSNP message
     </div>
   );
 };
