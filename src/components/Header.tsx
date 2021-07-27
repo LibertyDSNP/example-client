ad6aedf42cac73e9f490870ead06d95decab6c2e
shannonwells <shannonwells@users.noreply.github.com>
2021-07-27 16:25:16 -0700

    Remove ability to show New Post dialog, disable profile inputs, hard code username and handle in main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import * as wallet from "../services/wallets/wallet";
 const Header = (): JSX.Element => {
   return (
     <div className="Header__block">
-      <h1 className="Header__title">Example Client</h1>
+      <h1 className="Header__title">FOODEE</h1>
       <Login loginWalletOptions={wallet.WalletType.NONE} />
     </div>
   );
