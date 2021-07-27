66c96964df998e9621c606afed76c2f9e26c0c19
acruikshank <acruikshank@example.com>
2021-07-27 17:02:34 -0400

    clean up activityContent and remove activityPub types

--- src/utilities/activityContent.ts
+++ src/utilities/activityContent.ts
@@ -6,6 +6,24 @@ import {
   ActivityContentAttachment,
 } from "@dsnp/sdk/core/activityContent";
 
+const createMediaAttachment = (item: string): ActivityContentAttachment => {
+  const extension = path.extname(item).replace(".", "");
+  const supportedVideoDomains = /youtu.be|youtube.com|vimeo.com/;
+  if (supportedVideoDomains.test(item)) {
+    return createVideo(item, "text/html", { algorithm: "" }, 0, 0);
+  } else {
+    switch (extension) {
+      case "mp4":
+        return createVideo(item, "video/mp4", { algorithm: "" }, 0, 0);
+      case "wmv":
+        return createVideo(item, "video/x-ms-wmv", { algorithm: "" }, 0, 0);
+      case "mov":
+        return createVideo(item, "video/quicktime", { algorithm: "" }, 0, 0);
+    }
+    return createImage(item, `image/${extension}`, { algorithm: "" }, 0, 0);
+  }
+};
+
 export const noteToActivityContentNote = (
   note: string,
   uriList: string[]
@@ -17,47 +35,7 @@ export const noteToActivityContentNote = (
     published: new Date().getTime().toString(16),
   };
   if (uriList) {
-    if (typeof uriList === "string") {
-      uriList = [uriList];
-    }
-    activityContent["attachment"] = uriList.map(
-      (item): ActivityContentAttachment => {
-        const extension = path.extname(item).replace(".", "");
-        const supportedVideoDomains = /youtu.be|youtube.com|vimeo.com/;
-        if (supportedVideoDomains.test(item)) {
-          return createVideo(item, "text/html", { algorithm: "" }, 0, 0);
-        } else {
-          switch (extension) {
-            case "mp4":
-              return createVideo(item, "video/mp4", { algorithm: "" }, 0, 0);
-            case "wmv":
-              return createVideo(
-                item,
-                "video/x-ms-wmv",
-                { algorithm: "" },
-                0,
-                0
-              );
-            case "mov":
-              return createVideo(
-                item,
-                "video/quicktime",
-                { algorithm: "" },
-                0,
-                0
-              );
-            default:
-              return createImage(
-                item,
-                `image/${extension}`,
-                { algorithm: "" },
-                0,
-                0
-              );
-          }
-        }
-      }
-    );
+    activityContent["attachment"] = uriList.map(createMediaAttachment);
   }
   return activityContent;
 };
