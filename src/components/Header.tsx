1e1a87ded26933b3f27187ac306ba0d9e81abf17
V. Claire Olmstead <viol5229@colorado.edu>
2021-08-03 17:36:36 -0700

    generall styling added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,14 @@ import * as wallet from "../services/wallets/wallet";
 
 const Header = (): JSX.Element => {
   return (
-    <div className="Header__block">
-      <h1 className="Header__title">FOODEE</h1>
-      <Login loginWalletOptions={wallet.WalletType.NONE} />
+    <div className="Header__blockOuter">
+      <div className="Header__blockInner">
+        <div>
+          <h1 className="Header__title">Foodee</h1>
+          <h2 className="Header__subtitle">Always in the mood for food</h2>
+        </div>
+        <Login loginWalletOptions={wallet.WalletType.NONE} />
+      </div>
     </div>
   );
 };
