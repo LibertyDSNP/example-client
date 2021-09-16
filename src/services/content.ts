4a347c5c82a7519dfad267e2dfbc8bd90af87bec
Alex Cruikshank <169613+acruikshank@users.noreply.github.com>
2021-09-16 15:57:46 -0400

    authorize announcement messages before dispatching (#101)
    * authorize announcement messages before dispatching

--- src/services/content.ts
+++ src/services/content.ts
@@ -20,6 +20,7 @@ import {
   isGraphChangeAnnouncement,
   isProfileAnnouncement,
   isReplyAnnouncement,
+  isSignedAnnouncement,
 } from "@dsnp/sdk/core/announcements";
 import { BatchPublicationLogData } from "@dsnp/sdk/core/contracts/subscription";
 import { upsertGraph } from "../redux/slices/graphSlice";
@@ -29,6 +30,7 @@ import { HexString } from "@dsnp/sdk/types/Strings";
 import { DSNPAnnouncementURI, DSNPUserId } from "@dsnp/sdk/core/identifiers";
 import { FeedItem, User } from "../utilities/types";
 import { useQuery, UseQueryResult } from "react-query";
+import { Permission } from "@dsnp/sdk/core/contracts/identity";
 
 //
 // Content Package
@@ -257,9 +259,33 @@ const handleRegistryUpdate = (dispatch: Dispatch) => (
 const handleBatchAnnouncement = (dispatch: Dispatch) => (
   batchAnnouncement: BatchPublicationLogData
 ) => {
-  dsnp.readBatchFile(batchAnnouncement, (announcementRow, batchIndex) => {
+  dsnp.readBatchFile(batchAnnouncement, async (announcementRow, batchIndex) => {
     try {
       const announcement = announcementRow as unknown;
+
+      if (!isSignedAnnouncement(announcement)) {
+        console.log(
+          "batched announcement is not signed announcment",
+          announcement
+        );
+        return;
+      }
+
+      if (
+        !(await core.contracts.registry.isSignatureAuthorizedTo(
+          announcement.signature,
+          announcement,
+          BigInt(announcement.fromId),
+          Permission.ANNOUNCE
+        ))
+      ) {
+        console.log(
+          "batched announcment has unauthorized signature",
+          announcement
+        );
+        return;
+      }
+
       if (isGraphChangeAnnouncement(announcement)) {
         dispatch(
           upsertGraph({
