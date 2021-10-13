b4a0206d902ef2a3a6a1368a8903dfc03938c6e4
Wayland Li <wayland@Waylands-MacBook-Pro.local>
2021-10-13 11:01:17 -0700

    Removed part of error message as we have different types of errors now

--- src/services/errors.ts
+++ src/services/errors.ts
@@ -34,5 +34,5 @@ export const friendlyError = (
     return `${displayHandle} is not available. Please choose another.`;
   }
 
-  return "Try resetting your MetaMask account.\n" + message;
+  return message;
 };
