9c383a6ce87b5fd532123cfdf5d8c3d18318f4c1
V. Claire Olmstead <viol5229@colorado.edu>
2021-10-18 17:37:02 -0700

    thumbnails working. still need to fix tests

--- src/utilities/helpers.test.tsx
+++ src/utilities/helpers.test.tsx
@@ -1,8 +1,21 @@
+import React from "react";
 import * as helpers from "../utilities/helpers";
 
 describe("helpers", () => {
+  const setThumbnailMock = jest.fn();
+  const thumbnail = "";
+  const useStateMock: any = (thumbnail: string) => [
+    thumbnail,
+    setThumbnailMock,
+  ];
+  jest.spyOn(React, "useState").mockImplementation(useStateMock);
+
   it("returns youtube video thumbnail from link in nav bar", () => {
-    const thumbnail = helpers.getThumbnail(
+    helpers.getThumbnail(
+      "https://www.youtube.com/watch?v=gbYqlbZumc0",
+      setThumbnailMock
+    );
+    expect(setThumbnailMock).toHaveBeenCalledWith(
       "https://www.youtube.com/watch?v=gbYqlbZumc0"
     );
     expect(thumbnail).toEqual(
@@ -11,15 +24,16 @@ describe("helpers", () => {
   });
 
   it("returns youtube video thumbnail from share link", () => {
-    const thumbnail = helpers.getThumbnail("https://youtu.be/gbYqlbZumc0");
+    helpers.getThumbnail("https://youtu.be/gbYqlbZumc0", setThumbnailMock);
     expect(thumbnail).toEqual(
       "https://img.youtube.com/vi/gbYqlbZumc0/mqdefault.jpg"
     );
   });
 
   it("returns youtube video thumbnail from embedded link", () => {
-    const thumbnail = helpers.getThumbnail(
-      "https://www.youtube.com/embed/gbYqlbZumc0"
+    helpers.getThumbnail(
+      "https://www.youtube.com/embed/gbYqlbZumc0",
+      setThumbnailMock
     );
     expect(thumbnail).toEqual(
       "https://img.youtube.com/vi/gbYqlbZumc0/mqdefault.jpg"
@@ -27,21 +41,27 @@ describe("helpers", () => {
   });
 
   it("returns vimeo video thumbnail", () => {
-    const thumbnail = helpers.getThumbnail("https://vimeo.com/571191143");
+    helpers.getThumbnail("https://vimeo.com/571191143", setThumbnailMock);
     expect(thumbnail).toEqual("https://vumbnail.com/571191143.jpg");
   });
 
   it("returns img thumbnail", () => {
-    const thumbnail = helpers.getThumbnail(
-      "https://www.mccourt.com/siteFiles/Professionals/Braxton_12.20_BW.jpg"
+    helpers.getThumbnail(
+      "https://www.mccourt.com/siteFiles/Professionals/Braxton_12.20_BW.jpg",
+      setThumbnailMock
     );
     expect(thumbnail).toEqual(
       "https://www.mccourt.com/siteFiles/Professionals/Braxton_12.20_BW.jpg"
     );
   });
 
+  it("returns img thumbnail if no extension", () => {
+    helpers.getThumbnail("https://placekitten.com/300/300", setThumbnailMock);
+    expect(thumbnail).toEqual(undefined);
+  });
+
   it("returns undefined if no thumbnail is available", () => {
-    const thumbnail = helpers.getThumbnail("123");
+    helpers.getThumbnail("123", jest.fn);
     expect(thumbnail).toEqual(undefined);
   });
 });
--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,15 +1,25 @@
-export const getThumbnail = (url: string): string | undefined => {
-  const isYoutubeVideo = url.match(
-    /\/\/((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w]+)(\S+)?$/i
-  );
-  if (isYoutubeVideo)
-    return "https://img.youtube.com/vi/" + isYoutubeVideo[4] + "/mqdefault.jpg";
+export const getThumbnail = (
+  url: string,
+  setThumbnail: (url: string) => void
+): void => {
+  const thumbnailSrc = new Image();
+  thumbnailSrc.src = url;
+  thumbnailSrc.onload = function () {
+    console.log("onload", url);
+    return setThumbnail(url);
+  };
+  thumbnailSrc.onerror = function () {
+    console.log("onerror", url);
+    const isYoutubeVideo = url.match(
+      /\/\/((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w]+)(\S+)?$/i
+    );
+    if (isYoutubeVideo)
+      return setThumbnail(
+        "https://img.youtube.com/vi/" + isYoutubeVideo[4] + "/mqdefault.jpg"
+      );
 
-  const isVimeoVideo = url.match(/\/\/(?:www\.)?vimeo.com\/([0-9a-z\-_]+)/i);
-  if (isVimeoVideo) return "https://vumbnail.com/" + isVimeoVideo[1] + ".jpg";
-
-  const isImage = url.match(/\.(jpeg|jpg|gif|png|svg)$/);
-  if (isImage) return url;
-
-  return;
+    const isVimeoVideo = url.match(/\/\/(?:www\.)?vimeo.com\/([0-9a-z\-_]+)/i);
+    if (isVimeoVideo)
+      return setThumbnail("https://vumbnail.com/" + isVimeoVideo[1] + ".jpg");
+  };
 };
